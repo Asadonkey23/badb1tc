ad8fa73a3e0239c310af862fa99898b2e4856fca
Asadonkey23 <33288758+Asadonkey23@users.noreply.github.com>
2020-06-05 21:49:39 +0300

    *

--- btnet/Gucci (remastered)/loader/src/headers/config.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define HTTP_SERVER "0.0.0.0" //Buraya Kendi VPS Ip ni yazacan
-#define HTTP_PORT 80
-
-#define TFTP_SERVER "0.0.0.0" //Buraya Kendi VPS Ip ni yazacan
--- btnet/Private Mirai Meerkat/loader/src/headers/config.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define HTTP_SERVER "192.3.104.62"
-#define HTTP_PORT 80
-
-#define TFTP_SERVER "192.3.104.62"
