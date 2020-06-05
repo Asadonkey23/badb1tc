26bbc922a04524e06003d610b0069f4c7b65c073
Asadonkey23 <33288758+Asadonkey23@users.noreply.github.com>
2020-06-05 21:59:07 +0300

    f

--- btnet/FBI/loader/src/headers/config.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define HTTP_SERVER "80.211.108.225"
-#define HTTP_PORT 80
-
-#define TFTP_SERVER "80.211.108.225"
---
+++ btnet/riftbot2/loader/src/headers/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define HTTP_SERVER "1.2.3.4"
+#define HTTP_PORT 80
+
+#define TFTP_SERVER "1.2.3.4"
