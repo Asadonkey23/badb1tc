58823ef7e3e1c6f3c13385fed8d15a0fb76facd5
Asadonkey23 <33288758+Asadonkey23@users.noreply.github.com>
2020-06-05 21:09:20 +0300

    Delete **

--- btnet/Mana v4.1/telnet/src/headers/config.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define HTTP_SERVER "167.99.95.255"
-#define HTTP_PORT 80
-
-#define TFTP_SERVER "167.99.95.255"
--- btnet/Mana v4.1s/telnet/src/headers/config.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define HTTP_SERVER "162.216.114.40"
-#define HTTP_PORT 80
-
-#define TFTP_SERVER "162.216.114.40"
